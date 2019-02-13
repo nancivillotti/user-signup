722c3fe2afd3c3eb73ecd4cdd530e77a41b58cc1
Nanci Villotti <nancivillotti@hotmail.com>
2019-02-13 13:57:38 -0600

    fixed errors in username length and welcome page

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ def signin():
     if len(password)<3 or len(password)>20 or  (" " in password):
         password_error = "Enter valid password: password must be at least three characters and less than twenty characters. No spaces"
     
+    if len(username)<3 or len(username)>20 or  (" " in username):
+        username_error = "Enter valid username: username must be at least three characters and less than twenty characters. No spaces"
 
     if not password==verify:
         verify_error = "passwords must match. Do it over."
@@ -38,10 +40,11 @@ def signin():
     if not email == "":
         if not ("@" in email) or not ('.' in email) or len(email)<3 or len(email)>20:
             email_error = 'Enter valid email'
-    
 
-    return render_template('usersignup.html', email = email, username = username, 
-                            username_error = username_error, password_error= password_error,
-                            verify_error = verify_error, email_error = email_error)
+    if not username_error and not password_error and not verify_error and not email_error:
+        return render_template('welcome.html', username=username)
+    
+    else:
+        return render_template('usersignup.html', email = email, username = username, username_error = username_error, password_error= password_error, verify_error = verify_error, email_error = email_error)
 
 app.run()
